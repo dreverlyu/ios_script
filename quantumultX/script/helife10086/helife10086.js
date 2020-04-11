719c04de7d8423bac75a9ebedc4ef810ce6e42b2
DreverLyu <prettydongdong@outlook.com>
2020-04-11 22:49:01 +0800

    update

--- quantumultX/script/helife10086/helife10086.js
+++ quantumultX/script/helife10086/helife10086.js
@@ -4,28 +4,28 @@ const chen = init()
 let cookieVal = chen.getdata(cookieKey)
 sign()
 function sign() {
-    let url = {url:'http://he.sx.chinamobile.com',headers: { Cookie:cookieVal}}
+    let url = {url:'http://he.sx.chinamobile.com/h/rest/v1/sign/process',headers: { Cookie:cookieVal}}
     url.headers['Origin'] = 'http://he.sx.chinamobile.com'
-    url.headers['Connection'] = `keep-alive`
-    url.headers['Content-Type'] = ` application/json;charset=utf-8`
-    url.headers['Accept'] = `application/json, text/plain, */*`
-    url.headers['Host'] = `he.sx.chinamobile.com`
-    url.headers['User-Agent'] = ` Mozilla/5.0 (iPhone; CPU iPhone OS 13_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 oncon(iphone;13.3.1;com.MobileCommunicationSX.iHeYueSX;1.0.4) he`
-    url.headers['Accept-Language'] = `zh-cn`
-    url.headers['Accept-Encoding'] = ` gzip, deflate`
+    url.headers['Connection'] = 'keep-alive'
+    url.headers['Content-Type'] = 'application/json;charset=utf-8'
+    url.headers['Accept'] = 'application/json, text/plain, */*'
+    url.headers['Host'] = 'he.sx.chinamobile.com'
+    url.headers['User-Agent'] = 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 oncon(iphone;13.3.1;com.MobileCommunicationSX.iHeYueSX;1.0.4) he'
+    url.headers['Accept-Language'] = 'zh-cn'
+    url.headers['Accept-Encoding'] = 'gzip, deflate'
     chen.get(url, (error, response, data) => {
       const result = JSON.parse(data)
       const title = `${cookieName}`
-      let subTitle = ``
-      let detail = ``
+      let subTitle = ''
+      let detail = ''
     
       if (result.retCode == 0 && result.retMsg == 'OK') {
-        subTitle = `签到结果: 成功`
+        subTitle = '签到结果: 成功'
       } else if (result.resultCode == 3) {
-          subTitle = `签到结果: 失败,需要重新获得cookie`
+          subTitle = '签到结果: 失败,需要重新获得cookie'
       } else {
-        subTitle = `签到结果: 未 知`
-        detail = `说明: ${result.resultrMsg}`
+        subTitle = '签到结果: 未知'
+        detail = '说明: ${result.resultrMsg}'
       }
       chen.msg(title, subTitle, detail)
     })
