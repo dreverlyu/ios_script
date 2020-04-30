5664350e2fc903fb68517c901b8aa041235d4219
DreverLyu <prettydongdong@outlook.com>
2020-05-01 01:56:43 +0800

    update

--- script/heLife/helifeGetCookie.js
+++ script/heLife/helifeGetCookie.js
@@ -5,6 +5,7 @@ const cookieVal = $request.headers['Cookie']
 if (cookieVal) {
   if (chavy.setdata(cookieVal, cookieKey)) {
     chavy.msg(`${cookieName}`, '获取Cookie: 成功', '')
+    $prefs.valueForKey(cookieVal,cookieKey)
     console.log("heLifeCookie ==>"+cookieVal)
     chavy.log(`[${cookieName}] 获取Cookie: 成功, cookie: ${cookieVal}`)
   }
