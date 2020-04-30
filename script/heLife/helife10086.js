7f255136fd1d524bc4af37619743a6e2142a2f0f
DreverLyu <prettydongdong@outlook.com>
2020-04-30 17:53:06 +0800

    update

--- script/heLife/helife10086.js
+++ script/heLife/helife10086.js
@@ -1,55 +1,19 @@
-const userSignCheckinURL = 'http://he.sx.chinamobile.com/h/rest/v1/sign/process';
-const userSignCookieKey = 'heLifeSign_10086_userCookieKey';
-const userSignAgentKey = 'heLifeSign_10086_userAgentKey';
-const userSignDataKey = 'heLifeSign_10086_userDataKey';
-
-
-let isGetCookie = typeof ($request) !== 'undefined';
-if (isGetCookie) {
-    // 获取 Cookie
-    if ($request.headers['Cookie']) {
-        var cookie = $request.headers['Cookie'];
-        var userAgent = $request.headers['User-Agent'];
-        $prefs.setValueForKey(cookie, userSignCookieKey);
-        $prefs.setValueForKey(userAgent, userSignAgentKey);
-        $notify("成功获取山西和生活cookie 🎉", "", "请在Rewrite_Local禁用该脚本")
+var myRequest  = {
+    url: 'http://he.sx.chinamobile.com/h/rest/v1/sign/process',
+    method: 'post',
+    headers: {
+        "Cookie": $prefs.valueForKey("CookieHeLife"),
     }
-    $done({});
-} else {
-    // 山西移动和生活签到
-    var request = {
-        url: userSignCheckinURL,
-        method: 'POST',
-        headers: {
-            'Cookie': $prefs.valueForKey(userSignCookieKey),
-            'Accept-Encoding': 'gzip, deflate',
-            'xm-sign': '5eaf02499406c8e9f548ee374fcac69b(71)1582216331415(27)1582216331415',
-            'Connection': 'keep-alive',
-            'Host': 'he.sx.chinamobile.com',
-            'Accept': '*/*',
-            'Referer': 'http://he.sx.chinamobile.com/h/index.html',
-            'User-Agent': $prefs.valueForKey(userSignAgentKey),
-            'Content-type' : 'application/json;charset=utf-8',
-            'Content-Length': '19',
-            'Accept-Language': 'zh-cn'
-        },
-        body: JSON.stringify({})
-    };
+};
 
-    $task.fetch(request).then(response => {
-        const obj = JSON.parse(response.body);
-
-        if (obj.retCode == userSignDataKey) {
-            $notify("山西移动和生活", "", "重复签到");
-        } else {
-            $notify("山西移动和生活", "", "签到成功");
-        }
-        var temp = obj.retCode;
-        console.log(obj);
-        $prefs.setValueForKey(temp, userSignDataKey);
-        var tt = $prefs.valueForKey(userSignDataKey);
-        console.log(tt);
-    }, reason => {
-        $notify("山西移动和生活", "", reason.error)
-    });
-}
+$task.fetch(myRequest ).then(response => {
+    var body = JSON.parse(response.body);
+    console.log(response.body);
+    if (body.retMsg=="OK" && body.retCode=="0000"){
+        $notify("山西移动和生活" ,"签到成功")
+    }else{
+        $notify("山西移动和生活签到失败")
+    }
+}, reason => {
+    $notify("山西移动和生活签到. Interface error‼️‼️‼️", "", reason.error)
+});
--- script/heLife/helifeGetCookie.js
+++ script/heLife/helifeGetCookie.js
@@ -1,55 +1,24 @@
-const cookieName = '山西移动和生活'
-const cookieKey = 'sx_cookie_10086'
-const chen = init()
-const cookieVal = $request.headers['Cookie']
-if (cookieVal) {
-  if (chen.setdata(cookieVal, cookieKey)) {
-    chen.msg(`${cookieName}`, '获取Cookie: 成功', '')
-    chen.log(`[${cookieName}] 获取 Cookie: 成功, cookie: ${cookieVal}`)
-  }
-}
+var HE10086 = $request.headers["Cookie"];
 
-function init() {
-  isSurge = () => {
-    return undefined === this.$httpClient ? false : true
-  }
-  isQuanX = () => {
-    return undefined === this.$task ? false : true
-  }
-  getdata = (key) => {
-    if (isSurge()) return $persistentStore.read(key)
-    if (isQuanX()) return $prefs.valueForKey(key)
-  }
-  setdata = (key, val) => {
-    if (isSurge()) return $persistentStore.write(key, val)
-    if (isQuanX()) return $prefs.setValueForKey(key, val)
-  }
-  msg = (title, subtitle, body) => {
-    if (isSurge()) $notification.post(title, subtitle, body)
-    if (isQuanX()) $notify(title, subtitle, body)
-  }
-  log = (message) => console.log(message)
-  get = (url, cb) => {
-    if (isSurge()) {
-      $httpClient.get(url, cb)
+if (HE10086) {
+  if ($prefs.valueForKey("CookieHeLife") != undefined) {
+    if ($prefs.valueForKey("CookieHeLife") != HE10086) {
+      var cookie = $prefs.setValueForKey(HE10086, "CookieHeLife");
+      if (!cookie) {
+        $notify("更新山西移动和生活 Cookie失败‼️", "", "")
+      } else {
+        $notify("更新山西移动和生活 Cookie成功 🎉", "", "")
+      }
+    } else {
+      console.log("山西移动和生活 cookie", "无变化,无需获取")
     }
-    if (isQuanX()) {
-      url.method = 'GET'
-      $task.fetch(url).then((resp) => cb(null, {}, resp.body))
+  } else {
+    var cookie = $prefs.setValueForKey(WAPJ, "CookieYD");
+    if (!cookie) {
+      $notify("首次写入山西移动和生活 Cookie失败‼️", "", "")
+    } else {
+      $notify("首次写入山西移动和生活 Cookie成功 🎉", "", "")
     }
   }
-  post = (url, cb) => {
-    if (isSurge()) {
-      $httpClient.post(url, cb)
-    }
-    if (isQuanX()) {
-      url.method = 'POST'
-      $task.fetch(url).then((resp) => cb(null, {}, resp.body))
-    }
-  }
-  done = (value = {}) => {
-    $done(value)
-  }
-  return { isSurge, isQuanX, msg, log, getdata, setdata, get, post, done }
 }
-chen.done()
+$done({})
