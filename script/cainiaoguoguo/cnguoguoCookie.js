b2bafabe82cd636fe46549e4135c9efff3c3f19a
DreverLyu <prettydongdong@outlook.com>
2020-05-10 13:20:27 +0800

    cnguoguo

---
+++ script/cainiaoguoguo/cnguoguoCookie.js
@@ -0,0 +1,54 @@
+const cookieName = '菜鸟裹裹'
+const signHeaderKey = 'glory_signHeader_CNGUOGUO'
+const glory = init()
+  if (this.$request) {
+    const signHeaderVal = JSON.stringify($request.headers)
+    if (signHeaderVal) {
+      glory.setdata(signHeaderVal, signHeaderKey)
+      glory.msg(cookieName, `获取菜鸟裹裹Cookie: 成功`, ``)
+    }
+  }
+function init() {
+  isSurge = () => {
+    return undefined === this.$httpClient ? false : true
+  }
+  isQuanX = () => {
+    return undefined === this.$task ? false : true
+  }
+  getdata = (key) => {
+    if (isSurge()) return $persistentStore.read(key)
+    if (isQuanX()) return $prefs.valueForKey(key)
+  }
+  setdata = (key, val) => {
+    if (isSurge()) return $persistentStore.write(key, val)
+    if (isQuanX()) return $prefs.setValueForKey(key, val)
+  }
+  msg = (title, subtitle, body) => {
+    if (isSurge()) $notification.post(title, subtitle, body)
+    if (isQuanX()) $notify(title, subtitle, body)
+  }
+  log = (message) => console.log(message)
+  get = (url, cb) => {
+    if (isSurge()) {
+      $httpClient.get(url, cb)
+    }
+    if (isQuanX()) {
+      url.method = 'GET'
+      $task.fetch(url).then((resp) => cb(null, {}, resp.body))
+    }
+  }
+  post = (url, cb) => {
+    if (isSurge()) {
+      $httpClient.post(url, cb)
+    }
+    if (isQuanX()) {
+      url.method = 'POST'
+      $task.fetch(url).then((resp) => cb(null, {}, resp.body))
+    }
+  }
+  done = (value = {}) => {
+    $done(value)
+  }
+  return { isSurge, isQuanX, msg, log, getdata, setdata, get, post, done }
+}
+glory.done()
