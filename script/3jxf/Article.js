450ffcaebf76deb22cbf65392ebe6f70f4ab5741
DreverLyu <prettydongdong@outlook.com>
2020-05-21 23:23:01 +0800

    update

--- script/3jxf/Article.js
+++ script/3jxf/Article.js
@@ -9,7 +9,7 @@
  *
  */
 
-const appStartTime = new Date().getTime();
+const appEndTime = new Date().getTime();
 const articleId = Math.floor(Math.random()*10+1);
 const url = 'http://221.204.170.88:8184/app/personalCenter/articleTime?type=1&time=38&articleId='+articleId+'&appStartTime='+appStartTime;
 const method = 'POST';
@@ -23,7 +23,7 @@ const headers = {
     'Authorization' : 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHBpcmVUaW1lIjoxNTkwMTQzMjc3OTU3LCJ1c2VyQ29kZSI6MjUzNjcxMywiYWNjb3VudCI6IjE4MjM1MTUyMDcwIiwiYWNjb3VudFR5cGUiOjF9.a1u7bis0y0TIwktBsrLNIAlMZInWTgN4tcgxl6oj_uY',
     'Accept-Language' : 'zh-Hans-CN;q=1, zh-Hant-HK;q=0.9, en-CN;q=0.8'
 };
-const body = '{"appEndTime":"1590056908","appStartTime":"1590056904","type":"1","time":"4","articleId":"4"}';
+const body = '{"appEndTime":'+appEndTime+',"appStartTime":'+appEndTime-38+',"type":"1","time":"38","articleId":'+articleId+'}';
 
 const myRequest = {
     url: url,
@@ -33,6 +33,7 @@ const myRequest = {
 };
 
 $task.fetch(myRequest).then(response => {
+    console.log(body)
     console.log(response.body.code == 0 ? "阅读文章"+response.body.data: "阅读失败！！" );
 }, reason => {
     console.log(reason.error);
