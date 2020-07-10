8f0d4018a4a07ed6c47ae59531cd50d3c5cf8f34
DreverLyu <prettydongdong@outlook.com>
2020-07-10 22:16:40 +0800

    update

--- script/3jxf/question1.js
+++ script/3jxf/question1.js
@@ -1,22 +1,11 @@
 
 const url = "http://221.204.170.88:8184/app/question";
 const body = "{\"userId\":\"2536713\",\"method\":1,\"totalGrade\":\"100\",\"list\":[{\"selectAnswer\":\"A\",\"grade\":10,\"ifCorrect\":1,\"questionCode\":\"66c0b8b74e9e4f61961ac919cf93e7dc\",\"questionType\":1},{\"selectAnswer\":\"D\",\"grade\":10,\"ifCorrect\":1,\"questionCode\":\"cda80de538814c3f82dc9c4d4ac58396\",\"questionType\":1},{\"selectAnswer\":\"B\",\"grade\":10,\"ifCorrect\":1,\"questionCode\":\"372d271b929e4a6f95f38b4a92b05522\",\"questionType\":1},{\"selectAnswer\":\"B\",\"grade\":10,\"ifCorrect\":1,\"questionCode\":\"74b9ac532b364d7183d1ad2218f2e9ba\",\"questionType\":1},{\"selectAnswer\":\"C\",\"grade\":10,\"ifCorrect\":1,\"questionCode\":\"a091dca9b0c4453b979f10934ddec2a9\",\"questionType\":1},{\"selectAnswer\":\"C\",\"grade\":10,\"ifCorrect\":1,\"questionCode\":\"bfbde7cb0d9543cea6ba4faa6a3272c8\",\"questionType\":1},{\"selectAnswer\":\"C\",\"grade\":10,\"ifCorrect\":1,\"questionCode\":\"010ce1aaa59a4667ac3fe60a8d1d9eb8\",\"questionType\":1},{\"selectAnswer\":\"A\",\"grade\":10,\"ifCorrect\":1,\"questionCode\":\"4dc30f15bd0244568a07f96f00b88f4a\",\"questionType\":1},{\"selectAnswer\":\"B\",\"grade\":10,\"ifCorrect\":1,\"questionCode\":\"8de0983b86454b64b18a751217bebc23\",\"questionType\":3},{\"selectAnswer\":\"A\",\"grade\":10,\"ifCorrect\":1,\"questionCode\":\"cef67bd0d449407ca8323434f27365a2\",\"questionType\":1}],\"summaryCode\":\"1a40dba7b2d24acb9b6c9e5759eea871\"}";
-const headers = {
-    "Connection": "close",
-    "Accept-Encoding": "gzip, deflate",
-    "Content-Type": "application/json",
-    "Origin": "http://sxzhdjkhd.sxdygbjy.gov.cn:8081",
-    "userId": "2536713",
-    "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148",
-    "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHBpcmVUaW1lIjoxNTkxNzE0ODMyNjgzLCJ1c2VyQ29kZSI6MjUzNjcxMywiYWNjb3VudCI6IjE4MjM1MTUyMDcwIiwiYWNjb3VudFR5cGUiOjF9.n8qf5VNU8XkT5ulq43FBSYzO_C2oN5si8w3iaK4m1yk",
-    "Host": "221.204.170.88:8184",
-    "Referer": "http://sxzhdjkhd.sxdygbjy.gov.cn:8081/zhdj-pre/",
-    "Accept-Language": "zh-cn",
-    "Accept": "*/*"
-};
+const questionHeaderVal = $prefs.valueForKey('glory_header_3jxf');
+
 const request = {
     url: url,
-    headers: headers,
+    headers: questionHeaderVal,
     method: "post",
     body: body
 };
--- script/3jxf/questionSubject.js
+++ script/3jxf/questionSubject.js
@@ -1,23 +1,11 @@
 
 const url = "http://221.204.170.88:8184/app/question";
 const body = "{\"userId\":\"2536713\",\"method\":4,\"totalGrade\":\"90\",\"list\":[{\"selectAnswer\":\"A-B-D\",\"grade\":10,\"ifCorrect\":1,\"questionCode\":\"38ee82960508481386eda038be97e415\",\"questionType\":2},{\"selectAnswer\":\"B-C-D\",\"grade\":10,\"ifCorrect\":1,\"questionCode\":\"bed556dbcc6f4d3f8af1772e3e2e4c47\",\"questionType\":2},{\"selectAnswer\":\"A\",\"grade\":10,\"ifCorrect\":1,\"questionCode\":\"971fada4ec734313ab36a2dd4db93dd4\",\"questionType\":1},{\"selectAnswer\":\"A\",\"grade\":10,\"ifCorrect\":1,\"questionCode\":\"aa13f82230ab4bef82c3377feaf2e904\",\"questionType\":1},{\"selectAnswer\":\"A\",\"grade\":10,\"ifCorrect\":1,\"questionCode\":\"cef67bd0d449407ca8323434f27365a2\",\"questionType\":1},{\"selectAnswer\":\"A-B-C-E\",\"grade\":10,\"ifCorrect\":1,\"questionCode\":\"6e47c7f782af4596a3b9274f20f6c0cf\",\"questionType\":2},{\"selectAnswer\":\"A\",\"grade\":10,\"ifCorrect\":1,\"questionCode\":\"04114cabccec481aa2b7326227fe974a\",\"questionType\":1},{\"selectAnswer\":\"B\",\"grade\":10,\"ifCorrect\":1,\"questionCode\":\"c21545c1d7f24c3aaa6e16ac829178c7\",\"questionType\":1},{\"selectAnswer\":\"C\",\"grade\":10,\"ifCorrect\":1,\"questionCode\":\"a2274a9fee57424a84edc32ee4edc353\",\"questionType\":1},{\"selectAnswer\":\"A\",\"grade\":10,\"ifCorrect\":2,\"questionCode\":\"c3a6d72dd05146bf9f267bee41637bcf\",\"questionType\":1}],\"summaryCode\":\"57782847872c4936a7d718472a36738g\"}";
-const headers = {
-    "Connection": "close",
-    "Accept-Encoding": "gzip, deflate",
-    "Content-Type": "application/json",
-    "Origin": "http://sxzhdjkhd.sxdygbjy.gov.cn:8081",
-    "userId": "2536713",
-    "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148",
-    "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHBpcmVUaW1lIjoxNTkxNzE0ODMyNjgzLCJ1c2VyQ29kZSI6MjUzNjcxMywiYWNjb3VudCI6IjE4MjM1MTUyMDcwIiwiYWNjb3VudFR5cGUiOjF9.n8qf5VNU8XkT5ulq43FBSYzO_C2oN5si8w3iaK4m1yk",
-    "Host": "221.204.170.88:8184",
-    "Referer": "http://sxzhdjkhd.sxdygbjy.gov.cn:8081/zhdj-pre/",
-    "Accept-Language": "zh-cn",
-    "Accept": "*/*",
-    "Content-Length": "1251"
-};
+
+const questionSubjectHeaderVal = $prefs.valueForKey('glory_header_3jxf');
 const request = {
     url: url,
-    headers: headers,
+    headers: questionSubjectHeaderVal,
     method: "post",
     body: body
 };
