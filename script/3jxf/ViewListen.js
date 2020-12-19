9da641da217f93d793464c82d4a861e01d63a8da
DreverLyu <prettydongdong@outlook.com>
2020-12-19 22:37:10 +0800

    update

--- script/3jxf/ViewListen.js
+++ script/3jxf/ViewListen.js
@@ -1,16 +1,16 @@
 
 const viewListenHeaderVal =  {
-    'Accept': '*/*',
-    'Accept-Encoding': 'gzip, deflate',
-    'Accept-Language': 'zh-Hans-CN;q=1, zh-Hant-HK;q=0.9, en-CN;q=0.8',
-    'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHBpcmVUaW1lIjoxNjA3MzQ2MDQzMTk2LCJ1c2VyQ29kZSI6MjUzNjcxMywiYWNjb3VudCI6IjE4MjM1MTUyMDcwIiwiYWNjb3VudFR5cGUiOjF9.V5eWpyXD_putSEihYfiYlfPantpypDpYyQHAiVH0aeM',
-    'Connection': 'close',
-    'Content-Type': 'application/json',
-    'Host': '221.204.170.88:8184',
-    'User-Agent': 'SJXF/3.3.4 (iPhone; iOS 13.3.1; Scale/3.00)',
-    'sUserId': '2536713',
-    'signature': '2FC2E93BEC34ABDD545D297939F7274B',
-    'version': '3.3.4'
+    'Accept': `*/*`,
+    'Accept-Encoding': `gzip, deflate`,
+    'Accept-Language': `zh-Hans-CN;q=1, zh-Hant-HK;q=0.9, en-CN;q=0.8`,
+    'Authorization': `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHBpcmVUaW1lIjoxNjA3MzQ2MDQzMTk2LCJ1c2VyQ29kZSI6MjUzNjcxMywiYWNjb3VudCI6IjE4MjM1MTUyMDcwIiwiYWNjb3VudFR5cGUiOjF9.V5eWpyXD_putSEihYfiYlfPantpypDpYyQHAiVH0aeM`,
+    'Connection': `close`,
+    'Content-Type': `application/json`,
+    'Host': `221.204.170.88:8184`,
+    'User-Agent': `SJXF/3.3.4 (iPhone; iOS 13.3.1; Scale/3.00)`,
+    'sUserId': `2536713`,
+    'signature': `2FC2E93BEC34ABDD545D297939F7274B`,
+    'version': `3.3.4`
     };
 
 const method = 'POST';
@@ -27,7 +27,7 @@ const url = 'http://221.204.170.88:8184/app/businessCore';
 const myRequest = {
     url: url,
     method: method,
-    headers: JSON.stringify(viewListenHeaderVal),
+    headers: viewListenHeaderVal,
     body: body
 };
 
